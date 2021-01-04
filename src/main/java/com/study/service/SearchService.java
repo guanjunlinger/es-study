c16e9f1bac095a7f14305074de91bee016bdd062
guanjun <1126964622@qq.com>
2021-01-04 08:43:33 +0800

    搜索请求

---
+++ src/main/java/com/study/service/SearchService.java
@@ -0,0 +1,8 @@
+package com.study.service;
+
+import java.util.List;
+
+public interface SearchService {
+    List<String> searchByAddress(String address);
+
+}
---
+++ src/main/java/com/study/service/impl/SearchServiceImpl.java
@@ -0,0 +1,14 @@
+package com.study.service.impl;
+
+import com.study.service.SearchService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchServiceImpl implements SearchService {
+    @Override
+    public List<String> searchByAddress(String address) {
+        return null;
+    }
+}
